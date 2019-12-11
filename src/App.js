9fc3f4941f92b3ac20036e0976c07aaa0fdb9c17
TV321 <tvlahovic7@gmail.com>
2019-12-11 22:56:15 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.sass';
-import Card from './Card';
 import Head from './Head';
+import CardList from './CardList';
 
-function App() {
-  return (
-      <React.Fragment>
-            <Head />
-            <div className="container">
-                <Card />
-                <Card />
-                <Card />
-            </div>
+class App extends Component {
 
-      </React.Fragment>
-  );
+    state = {
+        pokemons: []
+    }
+
+    componentDidMount() {
+        fetch('https://pokeapi.co/api/v2/pokemon/')
+            .then(resp => resp.json())
+            .then(res => {
+                this.setState( { pokemons: res.results })
+            })
+    }
+
+    render() {
+        const { pokemons } = this.state;
+        const starters = pokemons.filter(poke => {
+            if(poke.name === "bulbasaur" || poke.name === "charmander" ||
+                poke.name === "squirtle") {
+                return poke
+            } else {
+                return null
+            }
+
+        })
+        console.log(starters)
+        return (
+
+            <React.Fragment>
+                  <Head />
+                  <div className="container">
+                      <CardList />
+                  </div>
+            </React.Fragment>
+        );
+    }
+
 }
 
 export default App;
---
+++ src/CardList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Card from './Card';
+
+
+const CardList = () =>{
+    return(
+        <React.Fragment>
+            <Card />
+            <Card />
+            <Card />
+        </React.Fragment>
+    );
+}
+
+
+export default CardList
