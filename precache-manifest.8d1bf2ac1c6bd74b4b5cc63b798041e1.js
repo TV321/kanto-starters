fc518fd7e49a1aa09150929cfbb22fc0fab8c173
TV321 <tvlahovic7@gmail.com>
2019-12-14 17:37:17 +0100

    Updates

---
+++ precache-manifest.8d1bf2ac1c6bd74b4b5cc63b798041e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6ad14b8b3a94d8d70fa406f95280cd",
+    "url": "/kanto-starters/index.html"
+  },
+  {
+    "revision": "b53cb276196370a8e618",
+    "url": "/kanto-starters/static/css/main.65c83721.chunk.css"
+  },
+  {
+    "revision": "d828e4cd82967c53c85e",
+    "url": "/kanto-starters/static/js/2.27729ffd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kanto-starters/static/js/2.27729ffd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b53cb276196370a8e618",
+    "url": "/kanto-starters/static/js/main.7b26277d.chunk.js"
+  },
+  {
+    "revision": "53adf4f6776e743b6e91",
+    "url": "/kanto-starters/static/js/runtime-main.a75a3b3c.js"
+  }
+]);
